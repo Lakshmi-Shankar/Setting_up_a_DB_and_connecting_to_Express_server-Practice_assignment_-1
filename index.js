e9386a5d5b6cd0ea79f746e9a9db2b2a0b3bf774
Lakshmi-Shankar <lakshmi7708671565@gmail.com>
2025-02-13 09:41:53 +0530

    Assignment Completed

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const { resolve } = require('path');
+const Mongoose = require("mongoose")
+require("dotenv").config();
 
 const app = express();
 const port = 3010;
@@ -9,6 +11,12 @@ app.use(express.static('static'));
 app.get('/', (req, res) => {
   res.sendFile(resolve(__dirname, 'pages/index.html'));
 });
+
+
+const MONGO_URI = process.env.MONGO_URI;
+Mongoose.connect(MONGO_URI)
+.then(() => console.log("MongoDB successfully connected."))
+.catch((err) => console.log("Error found:",err))
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
